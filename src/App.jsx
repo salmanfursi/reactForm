c41c3aa80e0d8aa5265520bc9964fada810c878a
unknown <salmanfursipro@gmail.com>
2024-05-04 00:44:53 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { CarouselDemo } from './components/carousel/CarouselDemo'
+import { Button } from './components/ui/button'
+
+
+function App() {
+
+  return (
+    <div className='px-20'>
+      <Button>hello shadcn</Button>
+      <CarouselDemo></CarouselDemo>
+    </div>
+  )
+}
+
+export default App
